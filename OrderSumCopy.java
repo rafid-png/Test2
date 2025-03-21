1245d0b301f85cf5bbaa8182a87dcb60e49f643d
Rafid Hossain Kazi <ex24279@qmul.ac.uk>
2025-03-21 12:59:54 +0000

    modified copy

--- OrderSumCopy.java
+++ OrderSumCopy.java
@@ -1,6 +1,7 @@
 public class OrderSum {
     public static void main(String[] args) {
-        System.out.println("Who's calling this");
+        System.out.println("Who's calling that");
+        System.out.println("Who's calling that");
     }
 }
 
